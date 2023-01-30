07285c696e6cb51d24a01d526d1c0fa4c5407ec5
agnieszkatab <agnieszkamandelt@gmail.com>
2023-01-30 21:06:59 +0100

    Przyciski/ funkcje/ obiekty

--- script.js
+++ script.js
@@ -9,10 +9,65 @@ function myColorChange() {
 }
 
 
+const paczka = {
+    name: "",
+    ulica: "",
+  };
+  
+  const paczka2 = {
+    name: "",
+    ulica: "",
+  };
+  
+  paczka2.waga = 20
+  paczka.name = "list"
+  paczka2.name = "paka"
 
 
 
 
-function mojaFunkcja (){
+
+function mojaFunkcja(id){
+    const element = document.getElementById("id01");
+const element2 = document.getElementById("id02");
+
+    if(id === 1){
+
+        informacja = "Typ twojej przesyłki to: " + paczka.name;
+        element.innerHTML = informacja;
     
+        
+        informacja = "Twoja paczka nie posiada wagi"
+        element2.innerHTML = informacja;
+        
+    }
+
+    else if(id === 2){
+        informacja = "Typ twojej przesyłki to: " + paczka2.name;
+        element.innerHTML = informacja;
+    
+        informacja = "Waga twojej przesyłki to: " + paczka2.waga + " kg";
+        element2.innerHTML = informacja;
+    }
+    
+   
+   
+  
+
 }
+
+
+
+
+
+
+
+
+  
+
+  
+  
+
+
+
+  
