2132220b80126e8720e87f82ad2e4ffd08319d1a
agnieszkatab <agnieszkamandelt@gmail.com>
2023-02-01 21:05:07 +0100

    zajęcia 5 funkcje itp

--- script.js
+++ script.js
@@ -59,13 +59,59 @@ const element2 = document.getElementById("id02");
 
 
 
-
-
-
-
   
 
+function processCall(recipient) {
+
+    const liczba = Math.random() > 0.5;
+
+    console.log(liczba);
+    console.log(!liczba);
   
+    if (!liczba) {
+      console.log("Abonent " + recipient + " jest niedostępny, zostaw wiadomość.");
+  
+    } else {
+      console.log(`Łączenie z ${recipient}, proszę czekać...`);
+  
+    }
+  }
+  
+  processCall("Mango");
+
+
+  function processCall(recipient, onAvailable, onNotAvailable) {
+    // Symuluj dostępność abonenta za pomocą liczby losowej
+      const isRecipientAvailable = Math.random() > 0.5;
+    
+      if (!isRecipientAvailable) {
+        onNotAvailable(recipient);
+        return;
+      }
+    
+      onAvailable(recipient);
+    }
+    
+    function takeCall(name) {
+      console.log(`Łączenie z ${name}, proszę czekać...`);
+    // Logika odbierania połączenia
+    }
+    
+    function activateAnsweringMachine(name) {
+      console.log(`Abonent ${name} jest niedostępny, zostaw wiadomość.`);
+    // Logika aktywacji automatycznej sekretarki
+    }
+    
+    function leaveHoloMessage(name) {
+      console.log(`Abonent ${name} jest niedostępny, nagrywamy hologram.`);
+    // Logika nagrywania hologramu
+    }
+    
+    processCall("Mango", takeCall, activateAnsweringMachine);
+    processCall("Poly", takeCall, leaveHoloMessage);
+  
+
+
   
 
 
